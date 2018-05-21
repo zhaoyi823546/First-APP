df70a3aabfe89f35807fb679fbee8b6b322c60cd
zhaoyi823546 <823546927@qq.com>
2018-05-21 23:28:16 +0800

    5.21

---
+++ latte-core/src/main/java/com/net/download/DownloadHandler.java
@@ -0,0 +1,11 @@
+package com.net.download;
+
+/**
+ * 项目名 FestEC2 on 2018/5/20.
+ * 包名   com.net.download
+ * 创建者   82354
+ * 创建时间   2018/5/20 22:49
+ * 描述  TODO
+ */
+public class DownloadHandler {
+}
